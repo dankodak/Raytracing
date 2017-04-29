227fd8210c89df3563b3680c98bb643bcc106c9c
Daniel Koch <dankodak@live.de>
2017-04-29 23:02:16 +0200

    Im Westen nichts Neues

--- objects.m
+++ objects.m
@@ -3,6 +3,8 @@ function a = objects(number)
 if number == 1
     %Sphaere
     a = @(x,y,z) (x+2).^2+(y+7).^2+(z-7).^2 -16;
+    %a = @(x,y,z) x.^2+y.^2+z.^2+2*x.*y.*z-1
+    %a = @(x,y,z) x.^2 + y.^2 + z.^2 -16;
 elseif number == 2
     %Torus
     a = @(x,y,z) (x.^2+y.^2+z.^2+16-4).^2-4.*16.*(y.^2+z.^2);
@@ -10,9 +12,11 @@ elseif number == 3
     %Rueckwand
     a = @(x,y,z) x + 12;
 elseif number == 4
-    
+    %Boden
+    a = @(x,y,z) y - 12;
 elseif number == 5
-    
+    %Decke
+    a = @(x,y,z) y + 12;
 elseif number == 6
     
 elseif number == 7
--- testscript.m
+++
@@ -1,5 +0,0 @@
-intersect(f,ray([7,0,0],[12,0,0]),[12,0,0])
-ray(testtest)
-ray nochn test
-Zweig test
-einen test
