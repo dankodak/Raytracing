7299ee3f2313518cbc78a1c22bbff0593ebb63f0
Daniel Koch <dankodak@live.de>
2017-05-07 14:34:50 +0200

    Doch nicht mehr normieren

--- ray.m
+++ ray.m
@@ -8,9 +8,9 @@ r = zeros(size(grid,1),size(grid,2),3);
 for i = 1:3
     r(:,:,i) = (grid(:,:,i) - eye(i));
 end
-for i=1:size(grid,1)
-    for j = 1:size(grid,2)
-        r(i,j,:) = squeeze(r(i,j,:))./norm(squeeze(r(i,j,:)));
-    end
-end
+% for i=1:size(grid,1)
+%     for j = 1:size(grid,2)
+%         r(i,j,:) = squeeze(r(i,j,:))./norm(squeeze(r(i,j,:)));
+%     end
+% end
 end
