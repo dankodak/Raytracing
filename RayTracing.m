b3a7fdf7727792c43441df4df02c91bb4b35b9fb
Daniel Koch <dankodak@live.de>
2017-05-09 17:18:35 +0200

    Fertig ohne Kommentare

--- RayTracing.m
+++ RayTracing.m
@@ -1,11 +1,11 @@
-function RayTracing(width, height, p, dist, eye, newton, rlight, amb, lamp, dir, amount_objects, spec, equations, rho, chess)
+function RayTracing(width, height, p, r1, r2, dist, eye, newton, rlight, amb, lamp, dir, amount_objects, spec, equations, rho, chess)
 a = str2func(equations{1})
 % width = 100;
 % height = 100;
 %p = [10;-2.5;-2.5];
 %dist = 0.05;
-r1 = [0;1;0];
-r2 = [0;0;1];
+% r1 = [0;1;0];
+% r2 = [0;0;1];
 % rlight=[-1;-1;0];
 % lamp=[16;7;7];
 % amb=[0.2,0.2,0.2];
